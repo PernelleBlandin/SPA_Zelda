71dbb7d3eedd1ed16426f1a7f1cc41ebc7a22aae
Asma Hachelef <asma.hachelef1@gmail.com>
2026-03-17 12:09:16 +0100

    Arborescence projet

---
+++ js/app.js
@@ -0,0 +1,1 @@
+import Home from './views/pages/Home.js';
---
+++ js/views/pages/Home.js
@@ -0,0 +1,9 @@
+export default class Home{
+    async render(){
+        let view = `
+        <h2> voici la page home </h2>
+        `;
+
+        return view;
+    }
+}
