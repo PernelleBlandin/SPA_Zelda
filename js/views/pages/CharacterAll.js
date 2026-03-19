2a787ee49290783372f1ea881e978ea498e99fc1
PernelleBlandin <pernelleblandin@gmail.com>
2026-03-19 18:48:43 +0100

    Ajout de css. Début mise en place icone pour la liste.

--- js/views/pages/CharacterAll.js
+++ js/views/pages/CharacterAll.js
@@ -3,9 +3,7 @@ import CharacterProvider from "../../services/CharacterProvider.js";
 export default class CharacterAll {
 
     async render () {
-        console.log("passage character all")
         let personnages = await CharacterProvider.fetchCharacters(10);
-        console.log(personnages);
         let view = `
         <h2>Tous les personnages</h2>
         <ul>
